e2218fb0f0235df93e42e90f3e7d2950df2c9578
Jeel Bhavsar <jeel@onesilq.com>
2025-10-07 12:06:41 +0530

    fix: resolve build issues

--- config/admin.ts
+++ config/admin.ts
@@ -1,20 +1,20 @@
 export default ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env("ADMIN_JWT_SECRET", "defaultAdminJWTSecret"),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env("API_TOKEN_SALT", "defaultApiTokenSalt"),
   },
   transfer: {
     token: {
-      salt: env('TRANSFER_TOKEN_SALT'),
+      salt: env("TRANSFER_TOKEN_SALT", "defaultTransferTokenSalt"),
     },
   },
   secrets: {
-    encryptionKey: env('ENCRYPTION_KEY'),
+    encryptionKey: env("ENCRYPTION_KEY", "defaultEncryptionKey"),
   },
   flags: {
-    nps: env.bool('FLAG_NPS', true),
-    promoteEE: env.bool('FLAG_PROMOTE_EE', true),
+    nps: env.bool("FLAG_NPS", true),
+    promoteEE: env.bool("FLAG_PROMOTE_EE", true),
   },
 });
