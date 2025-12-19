65e30bef7af0f74874a2c7edc945ed22adf59284
Jeel Bhavsar <jeel@onesilq.com>
2025-12-19 14:18:40 +0530

    update size limit for uploads to 10MB

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,12 +1,19 @@
 export default [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::logger",
+  "strapi::errors",
+  "strapi::security",
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::query",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
+  {
+    name: "strapi::body",
+    config: {
+      formidable: {
+        maxFileSize: 10 * 1024 * 1024, // 10MB in bytes
+      },
+    },
+  },
 ];
--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,7 @@
-export default () => ({});
+export default () => ({
+  upload: {
+    config: {
+      sizeLimit: 10 * 1024 * 1024, // 10MB in bytes
+    },
+  },
+});
